cc3e47b48c47521be260f02404031909a6c98982
李文榆 (Dora Lee) <dora80823@gmail.com>
2018-06-13 18:02:30 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,21 @@
-$(function(){
-  $('.content').hide();
-  $('#go').bind('click',(function(){
-    $.ajax({
-      type: 'GET',
-      url: 'https://data.kaohsiung.gov.tw/Opendata/DownLoad.aspx?Type=2&CaseNo1=AV&CaseNo2=2&FileType=1&Lang=C&FolderType=',
-      success: function(data) {
-        $('.content').show();
-        $('.store').html('');
-        var storeData = JSON.parse(data);
-        $.each(storeData, function(i, item){
-
-          if ($('#dist').val() == item.Zipcode.slice(0,3)) {
-            src = '<li>\
-                      <h4> ' + item.Name + '</h4>\
-                      <img src=' + item.Picture1 + ' ' + '>\
-                      <p> 地址 - ' + item.Add + '</p>\
-                      <p> 電話 - ' + item.Tel + '</p>\
-                      <p> 營業時間 - ' + item.Opentime + '</p>\
-                    </li>';
-            $('.store').append(src);
+$(function () {
+			 $('#go').on('click', function(){
+			 	$.ajax({
+			 		type: 'GET',
+			 		url: 'https://data.kcg.gov.tw/api/action/datastore_search?resource_id=92290ee5-6e61-456f-80c0-249eae2fcc97', 
+		         	dataType: 'json',
+		         	success: function (data) {
+		         		var storeData = data.result.records;
+					     $.each(storeData, function() {
+					     	if($('#dist').val() == this.Zone.slice(0,10)){
+					     		console.log(this)
+					     		list = '<li class="box">'+
+							            '<h4> ' + this.Name + '</h4>'+
+							            '<p> 地址 - ' + this.Add + '</p>'+
+							            '<p> 電話 - ' + this.Tel + '</p>'+
+							            '<p> 營業時間 - ' + this.Opentime + '</p>'+
+							            '</li>';
+							            $('.store').append(list);
           }
         });
         $('html, body').animate({
